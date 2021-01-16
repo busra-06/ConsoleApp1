f204284d1cdba2156a4585e747450d09562baaf4
BÜŞRA <busra.dogann06@gmail.com>
2021-01-17 00:27:04 +0300

    Add project files.

---
+++ Metodlar/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+
+
+namespace Metodlar
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Product urun1 = new Product();
+            urun1.Adi = "Elma";
+            urun1.Fiyati = 15;
+            urun1.Aciklama = "Amasya Elması";
+
+            Product urun2 = new Product();
+            urun2.Adi = "Karpuz";
+            urun2.Fiyati = 50;
+            urun2.Aciklama = "Diyarbakır Karpuzu";
+
+            Product[] urunler = new Product[] {urun1,urun2 };
+            foreach (Product urun in urunler)
+            {
+                Console.WriteLine(urun.Adi);
+                Console.WriteLine(urun.Fiyati);
+                Console.WriteLine(urun.Aciklama);
+                Console.WriteLine("--------------");
+
+            }
+
+            Console.WriteLine("------Metodlar-----");
+
+            //encapsulation
+            SepetManager sepetManager = new SepetManager();
+            sepetManager.Add(urun1);
+            sepetManager.Add(urun2);
+
+            sepetManager.Add2("Armut"," Yeşil Armut",12,20);
+        }
+    }
+}
+//Do Not Repeat Yourself- Clean Code- Best Practice
---
+++ Metodlar/SepetManager.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Metodlar
+{
+    class SepetManager
+    {
+        public void Add(Product urun)
+        {
+            Console.WriteLine("Sepete eklendi : "+ urun.Adi);
+         }
+
+        public void Add2(string urunAdi, string aciklama, double fiyat, int stokAdedi)
+        {
+            Console.WriteLine( urunAdi);
+        }
+    }
+}
